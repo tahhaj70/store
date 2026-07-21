5b4139d23c782fcd6e5568423d7a60505392541f
Muhammad Taha Javaid <mjavaid06@i2cinc.com>
2026-07-21 17:18:54 +0500

    Working With Repositories

--- src/main/java/com/thelinear/store/repositories/UserRepository.java
+++ src/main/java/com/thelinear/store/repositories/UserRepository.java
@@ -1,8 +1,12 @@
 package com.thelinear.store.repositories;
 
 import com.thelinear.store.entities.User;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository extends CrudRepository<User, Long> {
+
+    @Query("SELECT COUNT(u) FROM User u WHERE u.email = ?1")
+    public int countByEmail(String email);
 
 }
